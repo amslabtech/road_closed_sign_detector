3409bd8faa764ee933d9bb0d2aeb340b6eba32c5
naoya-sugiura <ee63052@meiji.ac.jp>
2019-07-08 21:48:58 +0900

    update

--- src/road_closed_sign_detector.cpp
+++ src/road_closed_sign_detector.cpp
@@ -6,12 +6,15 @@
 #include <pcl/filters/extract_indices.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/segmentation/extract_clusters.h>
+#include<pcl/filters/voxel_grid.h>
 
 class Color_cone_detector{
 	private:
 		ros::NodeHandle nh;
 		tf::TransformListener tflistener;
-		ros::Subscriber sub;
+		ros::Subscriber hokuyo_sub;
+		ros::Subscriber velodyne_sub;
+
 		ros::Publisher pub;
 		sensor_msgs::PointCloud2 cloud_input;
 		std::vector<sensor_msgs::PointCloud2> cloud_clusters_ros_;
@@ -19,36 +22,32 @@ class Color_cone_detector{
 	public:
 		Color_cone_detector();
 		void hokuyo_callback(const sensor_msgs::PointCloud2ConstPtr& msg);
-		void operate();
+		void velodyne_callback(const sensor_msgs::PointCloud2ConstPtr& msg);
 		void clusterKdTree(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_ptr, std::vector<pcl::PointIndices> &cluster_indices);
 		void extractCluster(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_pcl_ptr, std::vector<pcl::PointIndices> &cluster_indices);
 };
 
 Color_cone_detector::Color_cone_detector()
 {
-	vub = nh.subscribe("/hokuyo_obstacles", 1, &Color_cone_detector::hokuyo_callback, this);
+	hokuyo_sub = nh.subscribe("/hokuyo_obstacles", 1, &Color_cone_detector::hokuyo_callback, this);
+	velodyne_sub = nh.subscribe("/velodyne_obstacles", 1, &Color_cone_detector::velodyne_callback, this);
 	pub = nh.advertise<sensor_msgs::PointCloud2>("cluster_test",1);
-}
-
-void Color_cone_detector::operate()
-{
-
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_input_pcl_ptr (new pcl::PointCloud<pcl::PointXYZ>);
-	std::vector<pcl::PointIndices> cluster_indices;
-
-	pcl::fromROSMsg(cloud_input, *cloud_input_pcl_ptr);
-	clusterKdTree(cloud_input_pcl_ptr, cluster_indices);
-	extractCluster(cloud_input_pcl_ptr, cluster_indices);
 }
 
 void Color_cone_detector::clusterKdTree(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_ptr, std::vector<pcl::PointIndices> &cluster_indices)
 {
+	/*pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::VoxelGrid<pcl::PointXYZ> vg;
+	vg.setInputCloud(cloud_filtered_ptr);
+	vg.setLeafSize(0.09, 0.09, 0.09);
+	vg.filter(*cloud_filtered);
+	*/
 	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
 	tree->setInputCloud(cloud_filtered_ptr);
 	pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-	ec.setClusterTolerance(0.1);
-	ec.setMinClusterSize(5);
-	ec.setMaxClusterSize(60);
+	ec.setClusterTolerance(0.15);
+	ec.setMinClusterSize(30);
+	ec.setMaxClusterSize(900);
 	ec.setSearchMethod(tree);
 	ec.setInputCloud(cloud_filtered_ptr);
 	ec.extract(cluster_indices);
@@ -64,21 +63,34 @@ void Color_cone_detector::extractCluster(pcl::PointCloud<pcl::PointXYZ>::Ptr clo
 		extract.setIndices(inliers);
 		extract.setNegative(false);
 		extract.filter(*cloud_cluster_pcl_ptr);
+		std::cout << cloud_cluster_pcl_ptr->points.size() << std::endl;
 
 		sensor_msgs::PointCloud2 cloud_cluster_ros;
 		pcl::toROSMsg(*cloud_cluster_pcl_ptr, cloud_cluster_ros);
-		cloud_cluster_ros.header.frame_id = "base_link";
+		cloud_cluster_ros.header.frame_id = "velodyne";
 
 		pub.publish(cloud_cluster_ros);
+		
 	}
 
 }
 
+void Color_cone_detector::velodyne_callback(const sensor_msgs::PointCloud2ConstPtr &msg)
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_input_pcl_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+	std::vector<pcl::PointIndices> cluster_indices;
+
+	pcl::fromROSMsg(*msg, *cloud_input_pcl_ptr);
+	clusterKdTree(cloud_input_pcl_ptr, cluster_indices);
+	extractCluster(cloud_input_pcl_ptr, cluster_indices);
+
+}
 void Color_cone_detector::hokuyo_callback(const sensor_msgs::PointCloud2ConstPtr &msg)
 {
-	cloud_input = *msg;
+/*	cloud_input = *msg;
 	std::cout << "---subscribe---" << std::endl;
 	operate();
+*/
 }
 
 int main(int argc, char** argv)
